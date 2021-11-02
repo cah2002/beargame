ad17d56417f9896cf3d96fcf4d0662aa5fe4b3eb
cah2002 <91273689+cah2002@users.noreply.github.com>
2021-11-02 15:17:09 +0000

    Update game.js

--- game.js
+++ game.js
@@ -23,6 +23,7 @@ this.display = function() {
   bear = new Bear();
   document.addEventListener("keydown", moveBear, false);
 }
+  
  / Handle keyboad events // to move the bear function moveBear(e) {
 //codes of the four keys
 const KEYUP = 38; const KEYDOWN = 40; const KEYLEFT = 37; const KEYRIGHT = 39;
@@ -35,4 +36,46 @@ if (e.keyCode == KEYUP) { bear.move(0, -1)
 if (e.keyCode == KEYDOWN) {
 bear.move(0, 1)
 } // down key }
+   
+   
+this.fitBounds = function() {
+  let parent = this.htmlElement.parentElement;
+  let iw = this.htmlElement.offsetWidth; let ih = this.htmlElement.offsetHeight; let l = parent.offsetLeft;
+  let t = parent.offsetTop;
+  let w = parent.offsetWidth;
+  let h = parent.offsetHeight;
+  if (this.x < 0) this.x = 0;
+  if (this.x > w - iw) this.x = w - iw; if (this.y < 0) this.y = 0;
+  if (this.y > h - ih) this.y = h - ih; };
+   
+class Bee { constructor(beeNumber) {
+//the HTML element corresponding to the IMG of the bee
+this.htmlElement = createBeeImg(beeNumber);
+//iits HTML ID
+this.id = this.htmlElement.id;
+//the left position (x)
+this.x = this.htmlElement.offsetLeft; //the top position (y)
+this.y = this.htmlElement.offsetTop;
+this.move = function(dx, dy) { //move the bees by dx, dy
+this.x += dx; this.y += dy; this.display();
+};
+this.display = function() {
+//adjust position of bee and display it this.fitBounds();//add this to adjust to bounds 
+this.htmlElement.style.left = this.x + "px"; 
+this.htmlElement.style.top = this.y + "px"; 
+this.htmlElement.style.display = "block";
+};
+this.fitBounds = function() {
+//check and make sure the bees stays in the board space let parent = this.htmlElement.parentElement;
+let iw = this.htmlElement.offsetWidth;
+let ih = this.htmlElement.offsetHeight;
+let l = parent.offsetLeft;
+let t = parent.offsetTop;
+let w = parent.offsetWidth;
+let h = parent.offsetHeight;
+if (this.x < 0) this.x = 0;
+if (this.x > w - iw) this.x = w - iw;
+if (this.y < 0)
+this.y = 0;
+if (this.y > h - ih) this.y = h - ih;
 }
