bf891869e46b1a7df87d34448251d6a2dc0e63d2
cah2002 <91273689+cah2002@users.noreply.github.com>
2021-11-05 15:34:27 +0000

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ this.htmlElement = document.getElementById("bear");
 this.id = this.htmlElement.id;
 this.x = this.htmlElement.offsetLeft;
 this.y = this.htmlElement.offsetTop;
-  
+}  
 this.move = function(xDir, yDir) { 
   this.x += this.dBear * xDir;
   this.y += this.dBear * yDir;
@@ -14,7 +14,7 @@ this.display = function() {
   this.htmlElement.style.left = this.x + "px"; 
   this.htmlElement.style.top = this.y + "px"; 
   this.htmlElement.style.display = "absolute";
-};
+}
  function start() { //create bear
   bear = new Bear();
   document.addEventListener("keydown", moveBear, false);
